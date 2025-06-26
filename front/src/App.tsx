db46f100307cd35d0cf94bcd81d422b5440aa229
vfedorov <fedorovVL@gmail.com>
2025-06-26 17:46:00 +0200

    Add files via upload

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,31 +1,35 @@
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { LoginPage } from './pages/LoginPage';
 import { RoundListPage } from './pages/RoundListPage';
 import { RoundPage } from './pages/RoundPage';
+import { StoreProvider } from './stores';
 
-const ProtectedRoute = () => {
-    const { token } = useAuth();
-    return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
+const ProtectedRoute = observer(() => {
+  const { token } = useAuth();
+  return token ? <Outlet /> : <Navigate to='/login' replace />;
+});
 
-const App = () => {
-    return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
+const App = observer(() => {
+  return (
+    <StoreProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
 
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/rounds" element={<RoundListPage />} />
-                        <Route path="/rounds/:id" element={<RoundPage />} />
-                    </Route>
+            <Route element={<ProtectedRoute />}>
+              <Route path='/rounds' element={<RoundListPage />} />
+              <Route path='/rounds/:id' element={<RoundPage />} />
+            </Route>
 
-                    <Route path="*" element={<Navigate to="/rounds" replace />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
-};
+            <Route path='*' element={<Navigate to='/rounds' replace />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </StoreProvider>
+  );
+});
 
 export default App;
